5497177347117e803b8a15cb12589637a3e36220
William Yang <tinysqueak@gmail.com>
2016-08-06 23:59:57 -0700

    Fixed errors

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,20 +1,21 @@
-dir = ~/dotfiles
-olddir = ~/dotfiles_old
-files = ".vimrc"
+#!/bin/bash
 
-#echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-#mkdir -p $olddir
-#echo "done"
+dir=~/dotfiles
+# olddir = ~/dotfiles_old
+files=".vimrc"
+
+# echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+# mkdir -p $olddir
+# echo "done"
 
 echo -n "Changing to the $dir directory ..."
-mkdir $dir
 cd $dir
 echo "done"
 
 for file in $files; do
-    #echo "Moving any existing dotfiles from ~ to $olddir"
-    #mv ~/.$file ~/dotfiles_old/
+    # echo "Moving any existing dotfiles from ~ to $olddir"
+    # mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/$file
 done
 
