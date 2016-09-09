f565439ef4a8ab960ead5c30215e7f7bc31bdf1a
William Yang <willy.h.yang@gmail.com>
2016-09-09 13:33:55 -0700

    Added update script

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+(set -x; brew update;)
+
+(set -x; brew cleanup;)
+(set -x; brew cask cleanup;)
+
+brew upgrade
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+casks=( $(brew cask list) )
+
+for cask in ${casks[@]}
+do
+    installed="$(brew cask info $cask | grep 'Not installed')"
+
+    if [[ $installed = *[!\ ]* ]]; then
+        echo "${red}${cask}${reset} requires ${red}update${reset}."
+        (set -x; brew cask uninstall $cask --force;)
+        (set -x; brew cask install $cask --force;)
+    else 
+        echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
+    fi
+done
+
+vim +PluginUpdate
