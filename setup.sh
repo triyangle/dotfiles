e517b55d58a53661ea156c5b0e1628a9aee11bc2
William Yang <willy.h.yang@gmail.com>
2016-10-01 13:56:45 -0700

    Updated

--- setup.sh
+++ setup.sh
@@ -9,10 +9,3 @@ elif [ "$OS" == "Linux" ]; then
 fi
 
 bash vimsetup.sh
-
-if [ "$OS" == "Linux" ]; then
-    sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-    sudo update-alternatives --set editor /usr/bin/vim
-    sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-    sudo update-alternatives --set vi /usr/bin/vim
-fi
