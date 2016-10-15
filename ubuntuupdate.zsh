30c97e0fbb0ec3fa3cd938a28bd23a25d23c2391
William Yang <willy.h.yang@gmail.com>
2016-10-15 01:01:34 -0700

    Updated scripts

---
+++ ubuntuupdate.zsh
@@ -0,0 +1,49 @@
+#!/usr/bin/zsh
+
+echo -e "Updating system packages..."
+sudo apt-get update && sudo apt-get upgrade
+
+echo -e "\nUpdating vim..."
+cd ~/vim
+git pull
+
+sudo make uninstall
+
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --enable-python3interp \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+            --enable-perlinterp \
+            --enable-luainterp \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+
+make && sudo make install
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
+
+echo -e "\nYouCompleteMe setup..."
+cd ~/.vim/bundle/YouCompleteMe
+
+./install.py --clang-completer
+
+echo -e "\nUpdating vim plugins..."
+/usr/bin/vim +PluginUpdate +qall
+
+echo -e "\nUpdating prezto..."
+cd ~/.zprezto
+git pull && git submodule update --init --recursive
+
+echo -e "\nUpdating Powerline fonts..."
+cd ~/fonts
+git pull
+
+echo -e "\nCleaning..."
+sudo apt-get autoclean
+sudo apt-get autoremove
+sudo apt-get clean
