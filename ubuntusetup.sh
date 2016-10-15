290ff33f5a1bb34536b862d98fedb8ce708c5213
William Yang <willy.h.yang@gmail.com>
2016-10-14 23:28:57 -0700

    Updated ubuntu scripts

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -64,9 +64,10 @@ sudo apt-get install zsh
 echo -e "\nInstalling prezto..."
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 zsh ~/dotfiles/prezto.zsh
-chsh -s /usr/local/bin/zsh
+chsh -s /usr/bin/zsh
 
 echo -e "\nInstalling Powerline fonts..."
+cd ~
 git clone https://github.com/powerline/fonts.git
 cd ~/fonts
 ./install.sh
