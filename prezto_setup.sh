878f1e9665eff5a74bc230798352a6b69a990f62
William Yang <willy.h.yang@gmail.com>
2017-01-14 18:50:40 -0800

    Add remotes on setup

--- prezto_setup.sh
+++ prezto_setup.sh
@@ -1,6 +1,11 @@
 echo -e "\nInstalling prezto..."
 git clone --recursive https://github.com/triyangle/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 zsh ~/dotfiles/prezto.zsh
+cd ~/.zprezto
+git remote add upstream https://github.com/zsh-users/prezto.git
+
+cd ~/.zprezto/modules/prompt/external/statusline
+git remote add upstream https://github.com/el1t/statusline.git
 
 echo -e "\nInstalling Powerline fonts..."
 cd ~
