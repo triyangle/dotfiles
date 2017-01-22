96128d5b3e66cc12ea8ae2764dbe8f8226aa8f33
William Yang <willy.h.yang@gmail.com>
2017-01-22 15:42:15 -0800

    Added title and toolbar

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -27,6 +27,9 @@ cd $(jupyter --data-dir)/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
 chmod -R go-w vim_binding
 
+pip install jupyterthemes
+jt -t onedork -vim -T -N
+
 echo -e "\nInstalling vim..."
 brew install vim --with-custom-python
 
