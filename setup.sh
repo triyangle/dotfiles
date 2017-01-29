abd84c1328a9611466cca7449ae1242f2c7be095
William Yang <willy.h.yang@gmail.com>
2017-01-28 18:13:32 -0800

    Use filepaths for setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-bash symlink.sh
+bash ~/dotfiles/symlink.sh
 
 OS=`uname`
 
 if [ "$OS" == "Darwin" ]; then
-    bash macOS/setup.sh
+    bash ~/dotfiles/macOS/setup.sh
 elif [ "$OS" == "Linux" ]; then
-    bash ubuntu/setup.sh
+    bash ~/dotfiles/ubuntu/setup.sh
 fi
 
-bash prezto_setup.sh
-bash vimsetup.sh
+bash ~/dotfiles/jupyter_setup.sh
+
+bash ~/dotfiles/prezto_setup.sh
+bash ~/dotfiles/vimsetup.sh
