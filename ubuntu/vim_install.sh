1daf9f6f342779a248d2907061dae7ab3ed13eeb
William Yang <willy.h.yang@gmail.com>
2017-06-06 00:06:25 -0700

    Update vim config to python 3.6.1

--- ubuntu/vim_install.sh
+++ ubuntu/vim_install.sh
@@ -7,7 +7,7 @@ cd ~/vim
             --enable-pythoninterp \
             --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
             --enable-python3interp \
-            --with-python3-config-dir=/home/william/.linuxbrew/Cellar/python3/3.6.0/lib/python3.6/config-3.6m-x86_64-linux-gnu \
+            --with-python3-config-dir=/home/william/.linuxbrew/Cellar/python3/3.6.1/lib/python3.6/config-3.6m-x86_64-linux-gnu \
             --enable-perlinterp \
             --enable-luainterp \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
