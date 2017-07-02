3b44eee823770b18a1f25cd539d7b3b6aab57a8b
William Yang <willy.h.yang@gmail.com>
2017-07-02 13:43:29 -0700

    Update vim setup & config

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -21,7 +21,7 @@ echo -e "\nInstalling vim dependencies..."
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev git
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
 
 echo -e "\nRemoving default versions..."
 sudo apt-get remove vim vim-runtime gvim
