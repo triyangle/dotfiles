28774dc10f6b51571ce1a703ebe15a4e767764ba
William Yang <willy.h.yang@gmail.com>
2017-11-28 02:50:10 -0800

    Fix terminfo init

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
 
-tic -o ~/.terminfo ../config/tmux/tmux-256color.terminfo
+tic -o ~/.terminfo ~/dotfiles/config/tmux/tmux-256color.terminfo
