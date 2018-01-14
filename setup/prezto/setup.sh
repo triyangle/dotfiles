2482a19cddb0a006b21103f8b509c02a6324e877
William Yang <willy.h.yang@gmail.com>
2018-01-14 00:20:50 -0800

    fix crontab and prezto init filepaths

--- setup/prezto/setup.sh
+++ setup/prezto/setup.sh
@@ -2,7 +2,7 @@
 
 echo -e "\nInstalling prezto..."
 git clone --recursive https://github.com/triyangle/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-source ~/dotfiles/setup/init.zsh
+source ~/dotfiles/setup/prezto/init.zsh
 cd ~/.zprezto
 git remote set-url --add origin https://triyangle@bitbucket.org/triyangle/prezto.git
 git remote add upstream https://github.com/sorin-ionescu/prezto.git
