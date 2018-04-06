8081a60e3e4e01830615ba6744060226840abff3
William Yang <willy.h.yang@gmail.com>
2018-04-06 02:04:24 -0700

    clones ocf branch

--- setup/prezto/setup.sh
+++ setup/prezto/setup.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 echo -e "\nInstalling prezto..."
-git clone -b hive --recursive https://github.com/triyangle/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone -b ocf --recursive https://github.com/triyangle/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 source ~/dotfiles/setup/prezto/init.zsh
 cd ~/.zprezto
 git remote set-url --add origin https://triyangle@bitbucket.org/triyangle/prezto.git
 git remote add upstream https://github.com/sorin-ionescu/prezto.git
-git checkout ocf
 
 cd ~/.zprezto/modules/prompt/external/statusline
 git remote set-url --add origin https://triyangle@bitbucket.org/triyangle/statusline.git
