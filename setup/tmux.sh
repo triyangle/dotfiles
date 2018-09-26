a10098225a3cba35152f88dc6e1bc76b03d84105
William Yang <willy.h.yang@gmail.com>
2018-09-25 22:11:18 -0700

    cherry-pick e55be49d57ff72886274d5f5552ac08062225b1e

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -1,1 +1,2 @@
-tic -o ~/.terminfo ~/dotfiles/config/tmux/tmux-256color.terminfo
+tic -xo ~/.terminfo ~/dotfiles/config/tmux/xterm-256color-italic.terminfo
+tic -xo ~/.terminfo ~/dotfiles/config/tmux/tmux-256color.terminfo
