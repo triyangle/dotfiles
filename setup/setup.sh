18f2876334309477bb5070f5f78a699915744fa3
William Yang <william_yang@berkeley.edu>
2019-04-06 17:36:26 +0000

    enable vim setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -12,6 +12,6 @@ source ~/dotfiles/setup/prezto/setup.sh
 
 source ~/dotfiles/env/setup.sh
 
-# source ~/dotfiles/setup/vim.sh
+source ~/dotfiles/setup/vim.sh
 source ~/dotfiles/setup/tmux.sh
 # source ~/dotfiles/setup/jupyter/setup.sh
